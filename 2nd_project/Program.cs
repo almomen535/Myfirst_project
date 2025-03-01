302a7e82e516407099db975527a59a0ff7834f41
Al Momen <almomen535@gmail.com>
2025-03-01 16:36:34 +0600

    Added  a solution of a equation

--- 2nd_project/Program.cs
+++ 2nd_project/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("The ans of the equation is= " + (4 + 5 * 2));
         }
     }
 }
