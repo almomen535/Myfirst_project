6e7059a7d3ce56d191978e58ee9034712889b9cf
Al Momen <almomen535@gmail.com>
2025-03-02 23:03:40 +0600

    Added if..else conditional statements

--- 2nd_project/Program.cs
+++ 2nd_project/Program.cs
@@ -14,20 +14,28 @@ namespace _2nd_project
 
 
             // Operators ( assignment, relational, logical operators)
+
+            //Assignment operators
+            Console.WriteLine("This is assignment operators example: ");
             number1 += 5;
             Console.WriteLine($"Number is = {number1}");
-            Console.WriteLine(number1 > number2);
-
+           
             number1 += 22;
             Console.WriteLine($"Number is = {number1}");
+
+            // Relational operators
+            Console.WriteLine("This is relational operators example: ");
             Console.WriteLine(number1 > number2);
+            Console.WriteLine(number1 == number2);
+
+            // Logical operators
+
+            Console.WriteLine("This is logical operators example: ");
+            Console.WriteLine(33 > 11 && 1 < 9);
+            Console.WriteLine(24 < 11 || 4 > 1);
 
 
-
-
-
-
-
+            /*
             // User Input:
             Console.Write("Base of the triangle: ");
             x =  Convert.ToDouble(Console.ReadLine());
@@ -51,7 +59,37 @@ namespace _2nd_project
             area = pi * radius * radius;
             Console.WriteLine("Area of the Circle is = " + area);
 
+            */
 
+
+
+            // Conditional statements: if..else:
+
+            double number3;
+
+            Console.Write("Enter the value: ");
+            number3 = Convert.ToDouble(Console.ReadLine());
+
+            if(number3> 0)
+            {
+                Console.WriteLine("The number is Positive");
+            }
+
+            else if(number3<0)
+            {
+                Console.WriteLine("The number is Negative");
+            }
+
+
+            else
+            {
+                Console.WriteLine("The number is Zero");
+            }
+               
+
+
+            
+        
 
         }
     }
